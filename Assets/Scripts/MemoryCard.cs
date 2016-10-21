5d615eeb071ac6fea474ab6e159d86e4525233fc
Dan Finigan <danielfinigan@gmail.com>
2016-10-20 22:26:38 -0400

    added MemoryCard.cs script to make card back disappear when clicked

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryCard : MonoBehaviour {
+    [SerializeField]
+    private GameObject cardBack;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnMouseDown()
+    {
+        if (cardBack.activeSelf)
+            cardBack.SetActive(false);
+    }
+}
