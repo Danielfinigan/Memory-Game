c55c891d7daea8f90e1d5a7674dad42b01cc654b
Dan Finigan <danielfinigan@gmail.com>
2016-10-20 22:43:38 -0400

    Initialized SceneController Script to instantiate cards

--- Assets/Scripts/MemoryCard.cs
+++ Assets/Scripts/MemoryCard.cs
@@ -2,10 +2,23 @@
 using System.Collections;
 
 public class MemoryCard : MonoBehaviour {
-    [SerializeField]
-    private GameObject cardBack;
-	// Use this for initialization
-	void Start () {
+    [SerializeField]    private GameObject cardBack;
+    [SerializeField]    private SceneController controller;
+
+    private int _id;
+
+    public int id
+    {
+        get { return _id; }
+    }
+
+    public void SetCard(int id, Sprite image)
+    {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -2,10 +2,13 @@
 using System.Collections;
 
 public class SceneController : MonoBehaviour {
+    [SerializeField]    private MemoryCard originalCard;
+    [SerializeField]    private Sprite[] images;
 
 	// Use this for initialization
 	void Start () {
-	
+        int id = Random.Range(0, images.Length);
+        originalCard.SetCard(id, images[id]);
 	}
 	
 	// Update is called once per frame
